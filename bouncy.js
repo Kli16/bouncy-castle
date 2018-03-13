fd13d36c2e2c208072c08baffb5780ac726980af
Kevin Li <kevinli@dwightshrute.local>
2018-03-13 00:44:44 -0400

    changed stuff

--- bouncy.js
+++ bouncy.js
@@ -1,46 +1,57 @@
 var svg = document.getElementById("svg");
-var bounce_button = document.getElementById("bounce");
 var clear_button = document.getElementById("clear");
 
 var timerID = 0;
 
-var clear = function() {
-  svg.innerHTML = "";
-}
-
 var clear_btn = function() {
-  svg.innerHTML = "";
-  stopit();
+    while(svg.firstChild){
+	     svg.removeChild(svg.firstChild);
+    }
+    clearInterval(timerID);
 }
 
-var bounce = function(e){
-  clear();
-  var x = 250;
-  var y = 250;
+var circle = function(e){
+  clearInterval(timerID);
+  var c1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
   var xvel = Math.random() * 8 - 4;
   var yvel = Math.random() * 8 - 4;
-  var circle_bounce = function(){
-    clear();
-    var c1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    c1.setAttribute("cx", x);
-    c1.setAttribute("cy", y);
-    c1.setAttribute("r", "30");
-    c1.setAttribute("fill", "yellow");
-    c1.setAttribute("stroke", "yellow");
-    svg.appendChild(c1);
+  c1.setAttribute("cx", e.offsetX);
+  c1.setAttribute("cy", e.offsetY);
+  c1.setAttribute("xvel", xvel);
+  c1.setAttribute("yvel", yvel);
+  c1.setAttribute("r", "30");
+  c1.setAttribute("fill", "yellow");
+  c1.setAttribute("stroke", "yellow");
+  svg.appendChild(c1);
+}
+
+var circle_bounce = function(){
+  var x, y
+  for (var i = 0; i < svg.children.length; i++){
+    var circ = svg.children[i];
+    x = Number(circ.getAttribute("cx"));
+	  y = Number(circ.getAttribute("cy"));
+    xvel = Number(circ.getAttribute("xvel"));
+	  yvel = Number(circ.getAttribute("yvel"));
     x += xvel;
     y += yvel;
+	  circ.setAttribute("cx", x);
+	  circ.setAttribute("cy", y);
     if (x >= 470 || x <= 30) {
       xvel *= -1;
+      circ.setAttribute("xvel", xvel);
     }
     if (y <= 30 || y >= 470){
       yvel *= -1;
+      circ.setAttribute("yvel", yvel);
     }
   }
-  timerID = setInterval(circle_bounce, 15);
 }
 
-var stopit = function(){
-  clearInterval(timerID);
+var bounce = function(e){
+  circle(e)
+  timerID = setInterval(circle_bounce, 10);
 }
+
 clear_button.addEventListener('click', clear_btn);
+svg.addEventListener('click', bounce);
